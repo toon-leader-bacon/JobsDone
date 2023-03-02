3734fc5919f059d4a70ccdd8610d9fff51eac681
Arthur Bacon <bacon247@gmail.com>
2023-03-02 07:30:04 -0500

    Add install scripts for JobsDone command

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo mkdir -p /usr/local/nocab
+sudo cp jobs-done.mp3 /usr/local/nocab
+sudo cp JobsDone /usr/bin
+sudo cp JobsDone /usr/bin/jobsdone
---
+++ uninstall.sh
@@ -0,0 +1,2 @@
+sudo rm -r /usr/local/nocab
+sudo rm /usr/bin/JobsDone
